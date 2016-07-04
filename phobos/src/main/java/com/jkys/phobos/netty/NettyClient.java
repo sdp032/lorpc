1a4681b56f5bed26bd45337e6d5e4910b6afbc4c
haofu <haofu@91jkys.com>
2016-07-04 23:44:27 +0800

    add request,reponse,codec interface

---
+++ phobos/src/main/java/com/jkys/phobos/netty/NettyClient.java
@@ -0,0 +1,7 @@
+package com.jkys.phobos.netty;
+
+/**
+ * Created by frio on 16/7/4.
+ */
+public class NettyClient {
+}
---
+++ phobos/src/main/java/com/jkys/phobos/remote/URL.java
@@ -0,0 +1,8 @@
+package com.jkys.phobos.remote;
+
+/**
+ * we use URL to get server bind info,port info, ip info.etc
+ * Created by frio on 16/7/4.
+ */
+public class URL {
+}
